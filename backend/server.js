ed700e1ff47d128c8e10995558e5382e1309dfa8
da526 <da526@njit.edu>
2024-10-05 11:22:10 -0400

    gitignore, server, workouts

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config()
+
+const express = require("express")
+const mongoose = require("mongoose")
+const routes = require("./routes/workouts")
+
+const app = express()
+
+// For every request, look if it has a body
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+app.use("/api/workouts", routes)
+
+mongoose.connect(process.env.MONG_UI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log("connected to db & listening on port", process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
