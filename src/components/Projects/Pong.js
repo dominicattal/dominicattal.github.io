002e7af9f1a77ecfcdb0b5bbfbab2f6ed765dff0
da526 <da526@njit.edu>
2024-11-07 12:43:32 -0500

    updated pong

--- src/components/Projects/Pong.js
+++ src/components/Projects/Pong.js
@@ -3,11 +3,10 @@ function Pong() {
     <div className="card align-self-start" style={{ width: "18rem"}}>
       <div className="card-body">
         <h5 className="card-title">Pong</h5>
-        <p className="card-subtitle mb-2 text-body-secondary"><em>C, Git, OpenGL, GLFW, WinSock</em></p>
+        <p className="card-subtitle mb-2 text-body-secondary"><em>C, Git, OpenGL, GLFW</em></p>
         <p className="card-text">
-          Recreation of the classic video game Pong. This project is mainly
-          an exercise in socket programming. I plan to implement LAN and
-          online multiplayer.
+          Recreation of the classic video game Pong. User can play
+          side-by-side with another player on the same screen.
         </p>
         <a href="https://github.com/dominicattal/pong" class="card-link">Source</a>
       </div>
