9110ae60bc7af0bbb15bb7f82f04b80d9b6453b5
Dominic Attalienti <attalientidominic@gmail.com>
2026-05-30 19:31:18 -0400

    setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <main className="min-h-screen bg-zinc-950 text-white">
+      <Hero />
+    </main>
+  )
+}
+
+export default App
